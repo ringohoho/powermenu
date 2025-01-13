14781af05f92e4b5cbef8b220e38161a5d44db7c
Richard Chien <stdrc@outlook.com>
2025-01-13 20:42:18 +0800

    init

---
+++ powermenu/powermenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  powermenuApp.swift
+//  powermenu
+//
+//  Created by RC on 13/1/25.
+//
+
+import SwiftUI
+
+@main
+struct powermenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
